eb384ca353beef9c7828b2133b32662d3cf27c4a
Luis Enrique <luisenriquerodriguezvarela@gmail.com>
2026-05-03 15:40:17 -0600

    feat: migrate to native S3 lockfiles and add automated state bucket initialization to deployment workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "iac-infra-tfstate-bucket"
-    key            = "infra/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "iac-infra-tfstate-locks"
-    encrypt        = true
+    bucket       = "iac-infra-tfstate-bucket"
+    key          = "infra/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
